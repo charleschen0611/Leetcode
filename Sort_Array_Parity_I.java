25d43be6c5d58943b71d9bb6cb28bba5fd5d883e
Hongyi Chen <37935860+charleschen0611@users.noreply.github.com>
2019-07-10 23:25:30 -0400

    Update and rename Sort Array Parity I to Sort_Array_Parity_I.java

--- Sort_Array_Parity_I.java
+++ Sort_Array_Parity_I.java
@@ -1,4 +1,6 @@
 /*
+This code is faster than 100% of the submissions. 1ms! 
+
 Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
 
 You may return any answer array that satisfies this condition.
